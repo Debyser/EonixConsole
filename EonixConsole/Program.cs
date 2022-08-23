9eb2656844c49c94f77baef53e0fef5baaf35185
jdebyser <j.debyser@semlex.com>
2022-08-23 07:52:18 +0200

    Add project files.

---
+++ EonixConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EonixConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
