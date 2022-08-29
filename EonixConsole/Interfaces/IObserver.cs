978541a58011b6024d192bfb8abe2c7549bedfb9
jdebyser <j.debyser@semlex.com>
2022-08-29 17:42:23 +0200

    removed unecessary folders

--- EonixConsole/Interfaces/IObserver.cs
+++ EonixConsole/Interfaces/IObserver.cs
@@ -1,5 +1,8 @@
 ﻿namespace EonixConsole.Interfaces
 {
+    /// <summary>
+    /// Easier to adapt to another langage
+    /// </summary>
     public interface IObserver
     {
         void Update(ISubject subject, Trick trick);
